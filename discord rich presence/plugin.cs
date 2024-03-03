3ae0aa516ae05db77540d903b63d3bea86e1b40c
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2024-03-03 19:55:12 +0000

    hook level enter

--- discord rich presence/plugin.cs
+++ discord rich presence/plugin.cs
@@ -4,28 +4,41 @@ namespace Discord;
 
 public sealed class DiscordPlugin : Celeste64.Mod.GameMod {
 
-    DiscordRpcClient client = new("1204546983629561886", pipe: -1);
+    static DiscordRpcClient client;
+    static Timestamps now = Timestamps.Now;
+    static string level = "Level Select";
 
     public override void OnModLoaded() {
+        client = new("1204546983629561886", -1);
         client.Initialize();
-        client.SetPresence(new() {
-            State = "idle",
-            Assets = new Assets() {
-                LargeImageKey = "mountain",
-            }
-        });
+        SetPresence();
     }
-
-    public override void OnModUnloaded() => client.Dispose();
-
-    public override void OnMapLoaded(Celeste64.Map map) => client.SetPresence(new() {
-        State = "playing in " + map.Filename.Replace("Maps/", "").Replace(".map", ""),
-        Assets = new Assets() {
-            LargeImageKey = "mountain",
-        }
-    });
 
     public override void Update(float deltaTime) => client.Invoke();
 
+    [On.Celeste64.LevelInfo.Enter]
+    static void OnSelect(On.Celeste64.LevelInfo.orig_Enter orig, Celeste64.LevelInfo info, Celeste64.ScreenWipe wipe, float hold) {
+        orig(info, wipe, hold);
+        level = info.Name;
+        SetPresence();
+    }
+
+    static void SetPresence() => client.SetPresence(new() {
+        State = "Playing in " + level,
+        Assets = new Assets() {
+            LargeImageKey = "mountain",
+        },
+        Timestamps = now
+    });
+
+    // [On.Celeste64.Overworld.ctor]
+    // static void OnEnter(On.Celeste64.Overworld.orig_ctor orig, Celeste64.Overworld overworld, bool use_last_selected) {
+    //     orig(overworld, use_last_selected);
+    //     level = "Level Select";
+    //     SetPresence();
+    // }
+
+    public override void OnModUnloaded() => client.Dispose();
+    ~DiscordPlugin() => client.Dispose();
 }
 
